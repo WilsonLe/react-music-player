6c31dc044168be482a85bce520b821db431aa756
anantadhia <102227869+anantadhia@users.noreply.github.com>
2022-12-11 09:15:57 +0700

    tambah lagu

--- src/data.js
+++ src/data.js
@@ -2,65 +2,50 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
+      name: "Love Space",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
-      audio: "/Blow.mp3",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Love%20Space.mp3",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "Daylight",
+      name: "Blow",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
       artist: "Aiguille",
-      audio: "https://www.mboxdrive.com/Zutto%20Ishosha.mp3",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Blow.mp3",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Keep Going",
+      name: "Zutto Ishosha",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
-      audio: "https://www.mboxdrive.com/Neo%20Tokyo%20Rhapshody.mp3",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Zutto%20Ishosha.mp3",
       color: ["#CD607D", "#c94043"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Nightfall",
+      name: "Neo Tokyo Rhapsody",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
       artist: "Aiguille",
-      audio: "https://www.mboxdrive.com/Blow.mp3",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Neo%20Tokyo%20Rhapshody.mp3",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
     },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: false,
-    },
+
     //ADD MORE HERE
   ];
 }
