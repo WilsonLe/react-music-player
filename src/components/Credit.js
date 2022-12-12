c756f370248f3a2b386a548d41bfc293aebc46be
anantadhia <102227869+anantadhia@users.noreply.github.com>
2022-12-12 17:17:33 +0700

    crdt

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -1,34 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 const Credit = () => {
-	return (
-		<CreditContainer>
-			<Link href="https://www.wilsonle.me" target="_blank">
-				Made by Wilson
-			</Link>
-			<br></br>
-			<Link href="https://github.com/WilsonLe/react-music-player" target="_blank">
-				Github repository
-			</Link>
-		</CreditContainer>
-	);
+  return (
+    <CreditContainer>
+      <Link href="https://www.wilsonle.me" target="_blank">
+        Made by Wilson
+      </Link>
+      <br></br>
+      <Link
+        href="https://github.com/WilsonLe/react-music-player"
+        target="_blank"
+      >
+        Origin repository
+      </Link>
+      <Link href="https://github.com/anantadhia/reactplayer" target="_blank">
+        Tatsu repository by an2ta
+      </Link>
+    </CreditContainer>
+  );
 };
 
 const CreditContainer = styled.div`
-	user-select: none;
-	position: fixed;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-end;
-	justify-content: flex-end;
-	z-index: 12;
-	bottom: 5px;
-	right: 5px;
-	color: rgb(155, 155, 155);
-	font-size: 0.75rem;
+  user-select: none;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  z-index: 12;
+  bottom: 5px;
+  right: 5px;
+  color: rgb(155, 155, 155);
+  font-size: 0.75rem;
 `;
 
 const Link = styled.a`
-	color: rgb(155, 155, 155);
+  color: rgb(155, 155, 155);
 `;
 export default Credit;
